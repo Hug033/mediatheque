f2cc3eb5a760227c0e125371723b08f1d90a712d
hugob <hugo>
2020-05-05 09:49:38 +0200

    Detail

--- src/sample/FXML/AdminController.java
+++ src/sample/FXML/AdminController.java
@@ -3,6 +3,7 @@ package sample.FXML;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
